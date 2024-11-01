99e11411c455c41a05ed27481e2d6b697810a7e7
Mukul <mukul32sharma@gmail.com>
2024-11-01 14:03:19 +0530

    Day 1

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,12 +1,35 @@
 import express from "express";
+import { PrismaClient } from '@prisma/client'
 
 const app = express();
+const client = new PrismaClient();
+app.use(express.json());
 
-app.post("/hooks/catch/:userId/:zapId", (req, res) => {
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
     const userId = req.params.userId;
     const zapId = req.params.zapId;
+    const body = req.body;
 
-    // store it in db to a new trigger
+    await client.$transaction(async tx => {
+        // store it in db to a new trigger
+        const run = await tx.zapRun.create({
+            data: {
+                zapId: zapId, 
+                metadata: body
+            }
+        })
 
+        await tx.zapRunOutbox.create({
+            data: {
+                zapRunId: run.id,
+            }
+        })
+        
+    })
+    res.json({
+        message: "Webhook recieved"
+    })
     // push it on the queue (kafka/redis)
 })
+
+app.listen(3000)
